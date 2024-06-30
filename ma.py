7d264c3cc36166fe29e2cc78854e77023fae4456
RoundBearChoi <roundbeargames@gmail.com>
2024-07-01 01:50:51 +0900

    save

--- ma.py
+++ ma.py
@@ -9,10 +9,12 @@ def plot_moving_average(weeks):
     print('Calculating ' + str(weeks) + '-week moving average..')
 
     # Fetch daily price data for Bitcoin
+    days_limit = 1600
+
     data1 = cryptocompare.get_historical_price_day(
-        'BTC', currency='USD', limit=2000, toTs=datetime.now())
+        'BTC', currency='USD', limit=days_limit, toTs=datetime.now())
     data2 = cryptocompare.get_historical_price_day(
-        'BTC', currency='USD', limit=2000, toTs=datetime.now() - pd.DateOffset(days=2000))
+        'BTC', currency='USD', limit=days_limit, toTs=datetime.now() - pd.DateOffset(days=days_limit))
 
     # Convert lists to DataFrames
     df1 = pd.DataFrame(data1)
@@ -27,15 +29,17 @@ def plot_moving_average(weeks):
     df['moving_avg'] = df['close'].rolling(window=7 * weeks).mean()
 
     # Plot
+    plt.style.use('ggplot')
     plt.figure(figsize=(14, 7))
-    plt.plot(df['close'], label='Bitcoin Price')
-    plt.plot(df['moving_avg'], label=f'{weeks}-Week Moving Average')
+    plt.plot(df['moving_avg'], label=f'{weeks}-Week Moving Average', linewidth=1)
+    plt.plot(df['close'], label='Bitcoin Price', linewidth=1)
     plt.title('Bitcoin Price and Moving Average')
     plt.ylabel('Price (USD)')
     plt.legend()
-    plt.grid(True)
+    plt.grid(False)
     plt.show()
 
 
-# Call the function with the number of weeks you want
-plot_moving_average(140)
+if __name__ == '__main__':
+    # Call the function with the number of weeks you want
+    plot_moving_average(140)
